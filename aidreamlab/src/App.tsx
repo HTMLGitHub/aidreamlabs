03de411f622e9c735e2d9008224c30edd1cf059e
Matthew Lee <bak120405@gmail.com>
2026-07-07 20:50:16 -0400

    inital setup for react website

---
+++ aidreamlab/src/App.tsx
@@ -0,0 +1,13 @@
+// Main app component for the AIDreamLab landing page
+import brandIcon from "./assets/dazbog.png";
+
+export default function App()
+{
+  return(
+    <main>
+      <img src={brandIcon} alt="AIDreamLab Icon" width="48" height="48" />
+      <h1>AI Dream Lab</h1>
+      <p>Coming Soon</p>
+    </main>
+  )
+}
